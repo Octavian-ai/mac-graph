765cfde3ce4c76e6cf7cf2aa0d79c5e44d1d63b4
David Mack <david@sketchdeck.com>
2018-07-20 15:30:44 +0100

    known ok

---
+++ train-station-adj.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+COMMIT=$(git --no-pager log --pretty=format:'%h' -n 1)
+
+python -m mac-graph.train \
+	--input-dir input_data/processed/sa_small_100k_balanced \
+	--model-dir output/model/sa/$COMMIT \
+	--log-level DEBUG \
+	--disable-kb-node \
+	--disable-data-stack \
+	--memory-width 128
