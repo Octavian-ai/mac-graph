5f2cf5c06eb222c8b14cfe8198f71a1d766a6f71
David Mack <david@sketchdeck.com>
2018-07-24 16:50:05 +0100

    running floyd tests

--- floyd-train.sh
+++ floyd-train.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 
+RUNTIME=$(expr 60 \* 60 \* 1)
+
+
 floyd run \
-	--message "Baseline - early attn q dense + post scoring dense" \
-	--gpu \
+	--message "Baseline + 2 decode - indicator row" \
+	--cpu \
 	--tensorboard \
 	--env tensorflow-1.8 \
-    --data davidmack/datasets/mac-graph-station-adjacent:/input \
-    --max-runtime $(expr 60 \* 60 \* 1) \
-    "python -m mac-graph.train \
-    	--input-dir /input \
-    	--output-dir /output \
-    	--model-dir /output/model \
-        --disable-kb-node \
-        --max-decode-iterations 2 \
-        --max-steps 50000
-    "
+	--data davidmack/datasets/mac-graph-station-adjacent:/input \
+	--max-runtime $RUNTIME  \
+	"python -m mac-graph.train \
+		--input-dir /input \
+		--output-dir /output \
+		--model-dir /output/model \
+		--disable-kb-node \
+		--max-decode-iterations 2 \
+		--num-input-layers 1 \
+		--disable-memory-cell
+	"
