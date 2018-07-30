fixed nan bugs from softmax

mac-graph/attention.py
@@ -2,6 +2,7 @@
 from .util import *
 
 from .args import global_args
+from .const import EPSILON
 
 
 def softmax_with_masking(logits, mask, axis):
@@ -9,21 +10,26 @@ def softmax_with_masking(logits, mask, axis):
 	mask = dynamic_assert_shape(mask, tf.shape(logits))
 	assert axis < len(logits.shape)
 
-	l = tf.exp(logits) * tf.cast(mask, logits.dtype)
-	d = tf.reduce_sum(l, axis)
+	# For numerical stability shrink the values
+	logit_max = tf.reduce_max(tf.boolean_mask(logits, mask))
+
+	# Numerator
+	l = tf.exp(logits - logit_max) * tf.cast(mask, logits.dtype)
+	
+	# Denominator
+	d = tf.reduce_sum(l, axis) 
 	d = tf.expand_dims(d, axis)
 
-	return l / d
+	return l / (d + EPSILON)
 
 
-def attention(table, query, mask=None, word_size=None, table_len=None, table_max_len=None):
+def attention(table, query, word_size=None, table_len=None, table_max_len=None):
 	"""
 	Apply attention
 
 	Arguments:
 		- `query`, shape (batch_size, width)
 		- `table`, shape (batch_size, len, width)
-		- Optional `mask`, shape (batch_size, width)
 		- word_size: The width of the table entries
 		- `table_len` A tensor of the lengths of the tables (in the batch) that is used to mask the scores before applying softmax (i.e. meaning that any table values after the length are ignored in the lookup)
 
@@ -50,46 +56,13 @@ def attention(table, query, mask=None, word_size=None, table_len=None, table_max
 		q_shape = [batch_size, word_size]
 		scores_shape = [batch_size, seq_len, 1]
 
-
 		q = dynamic_assert_shape(q, q_shape)
-
-		if mask is not None:
-			mask = dynamic_assert_shape(mask, q_shape)
 
 		# --------------------------------------------------------------------------
 		# Run model
 		# --------------------------------------------------------------------------
-		
-		if mask is not None:
-			q  = q  * mask
-			db = db * tf.expand_dims(mask, 1)
-
-		# Ensure masking didn't screw up the shape
-		db = dynamic_assert_shape(db, db_shape)
 
 		scores = tf.matmul(db, tf.expand_dims(q, 2))
-
-		# A big effort to do a dense layer on scores (it has a dynamic width)
-		if global_args["use_attn_score_dense"]:
-			need_to_set_shape = scores.shape[1].value is None
-			assert word_size is not None, "Cannot use_dense with unknown width_size"
-			assert not need_to_set_shape or table_max_len is not None, f"Please supply max seq len since seq len {db.name} is dynamic"
-
-			scores = tf.squeeze(scores, axis=2)
-
-			if need_to_set_shape:
-				delta = table_max_len - seq_len
-				scores = tf.pad(scores, [[0, 0], [0, delta]])
-				scores = tf.reshape(scores, [-1, table_max_len])
-				
-			scores = tf.layers.dense(scores, word_size)
-
-			if need_to_set_shape:
-				scores = scores[:,0:seq_len]
-
-			scores = tf.expand_dims(scores, axis=2)
-			scores = dynamic_assert_shape(scores, scores_shape)
-
 
 		if table_len is not None:
 			scores_mask = tf.sequence_mask(table_len, seq_len)
@@ -107,5 +80,28 @@ def attention(table, query, mask=None, word_size=None, table_len=None, table_max
 		output = dynamic_assert_shape(output, q_shape)
 
 		return output, scores
+
+
+
+# A big effort to do a dense layer on scores (it has a dynamic width)
+# if global_args["use_attn_score_dense"]:
+# 	need_to_set_shape = scores.shape[1].value is None
+# 	assert word_size is not None, "Cannot use_dense with unknown width_size"
+# 	assert not need_to_set_shape or table_max_len is not None, f"Please supply max seq len since seq len {db.name} is dynamic"
+
+# 	scores = tf.squeeze(scores, axis=2)
+
+# 	if need_to_set_shape:
+# 		delta = table_max_len - seq_len
+# 		scores = tf.pad(scores, [[0, 0], [0, delta]])
+# 		scores = tf.reshape(scores, [-1, table_max_len])
+		
+# 	scores = tf.layers.dense(scores, word_size)
+
+# 	if need_to_set_shape:
+# 		scores = scores[:,0:seq_len]
+
+# 	scores = tf.expand_dims(scores, axis=2)
+# 	scores = dynamic_assert_shape(scores, scores_shape)
 		
 

mac-graph/cell/read_cell.py
@@ -8,7 +8,7 @@ from ..minception import *
 
 # TODO: Make indicator row data be special token
 
-def read_from_table(args, features, in_signal, noun, table, width, table_len, table_max_len, use_mask=False):
+def read_from_table(args, features, in_signal, noun, table, width, table_len, table_max_len):
 
 	if args["read_indicator_cols"] > 0:
 		ind_col = tf.get_variable(f"{noun}_indicator_col", [1, 1, args["read_indicator_cols"]])
@@ -19,13 +19,7 @@ def read_from_table(args, features, in_signal, noun, table, width, table_len, ta
 	# query = tf.layers.dense(in_signal, width, activation=tf.nn.tanh)
 	query = tf.layers.dense(in_signal, width)
 
-	if use_mask:
-		mask  = tf.layers.dense(in_signal, width, activation=tf.nn.tanh)
-	else:
-		mask = None
-
-
-	output, score = attention(table, query, mask, 
+	output, score = attention(table, query,
 		word_size=width, 
 		table_len=table_len,
 		table_max_len=table_max_len,