5dcfd44d436c3006a03fd4eb09e9a60e08083566
David Mack <david@sketchdeck.com>
2018-07-30 20:42:18 +0100

    two tasks

--- floyd-train.sh
+++ floyd-train.sh
@@ -1,89 +1,22 @@
 #!/bin/bash
 
-RUNTIME=$(expr 60 \* 60 \* 2)
+RUNTIME=$(expr 60 \* 60 \* 1)
 COMMIT=$(git --no-pager log --pretty=format:'%h' -n 1)
 
 
+
 floyd run \
-	--message "$COMMIT baseline without subtract dense(in_signal) prior to relu-abs" \
+	--message "$COMMIT station properties + station adjacency" \
 	--cpu \
 	--env tensorflow-1.8 \
-	--data davidmack/datasets/mac-graph-station-adjacent:/input \
-	--max-runtime $(expr 60 \* 60 \* 1)  \
+	--data davidmack/datasets/mac-graph-sa-sp:/input \
+	--max-runtime $RUNTIME \
 	"python -m mac-graph.train \
 		--input-dir /input \
 		--output-dir /output \
 		--model-dir /output/model \
-		--disable-kb-node --max-decode-iterations 1 --input-layers 1 --disable-memory-cell --read-indicator-rows 1 --disable-control-cell --disable-dynamic-decode --disable-question-state --read-dropout 0.0 \
-	"
-
-
-floyd run \
-	--message "$COMMIT baseline + dropout, dynamic decode, question state, control cell" \
-	--cpu \
-	--env tensorflow-1.8 \
-	--data davidmack/datasets/mac-graph-station-adjacent:/input \
-	--max-runtime $RUNTIME  \
-	"python -m mac-graph.train \
-		--input-dir /input \
-		--output-dir /output \
-		--model-dir /output/model \
-		--disable-kb-node \
 		--max-decode-iterations 1 \
 		--input-layers 1 \
-		--disable-memory-cell \
-		--learning-rate 2E-6 \
-		--max-gradient-norm 1.0 \
+		--answer-classes 86 \
+		--vocab-size 86 \
 	"
-
-floyd run \
-	--message "$COMMIT baseline + dropout, dynamic decode, question state, control cell, memory cell" \
-	--cpu \
-	--env tensorflow-1.8 \
-	--data davidmack/datasets/mac-graph-station-adjacent:/input \
-	--max-runtime $RUNTIME  \
-	"python -m mac-graph.train \
-		--input-dir /input \
-		--output-dir /output \
-		--model-dir /output/model \
-		--disable-kb-node \
-		--max-decode-iterations 1 \
-		--input-layers 1 \
-		--learning-rate 2E-6 \
-		--max-gradient-norm 1.0 \
-	"
-
-floyd run \
-	--message "$COMMIT baseline + dropout, dynamic decode, question state, control cell, memory cell, input-layers 3" \
-	--cpu \
-	--env tensorflow-1.8 \
-	--data davidmack/datasets/mac-graph-station-adjacent:/input \
-	--max-runtime $RUNTIME  \
-	"python -m mac-graph.train \
-		--input-dir /input \
-		--output-dir /output \
-		--model-dir /output/model \
-		--disable-kb-node \
-		--max-decode-iterations 1 \
-		--input-layers 3 \
-		--learning-rate 2E-6 \
-		--max-gradient-norm 1.0 \
-	"
-
-floyd run \
-	--message "$COMMIT baseline + dropout, dynamic decode, question state, control cell, memory cell, input-layers 3, decode iterations 4" \
-	--cpu \
-	--env tensorflow-1.8 \
-	--data davidmack/datasets/mac-graph-station-adjacent:/input \
-	--max-runtime $RUNTIME  \
-	"python -m mac-graph.train \
-		--input-dir /input \
-		--output-dir /output \
-		--model-dir /output/model \
-		--disable-kb-node \
-		--max-decode-iterations 4 \
-		--input-layers 3 \
-		--learning-rate 2E-6 \
-		--max-gradient-norm 1.0 \
-	"
-
--- train-experimental.sh
+++ train-experimental.sh
@@ -3,8 +3,11 @@
 COMMIT=$(git --no-pager log --pretty=format:'%h' -n 1)
 
 python -m mac-graph.train \
-	--input-dir input_data/processed/sa_small_100k_balanced \
+	--input-dir input_data/processed/sa-sp-small-100k \
 	--model-dir output/model/sa/$COMMIT \
 	--log-level DEBUG \
-	--max-decode-iterations 1 \
-	--input-layers 1 \
+	--max-decode-iterations 3 \
+	--input-layers 3 \
+	--answer-classes 86 \
+	--vocab-size 86 \
+	--enable-tf-debug \
