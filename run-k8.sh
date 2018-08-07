0d2a6b695c11f04badc5d7758067d95537cf028f
David Mack <david@sketchdeck.com>
2018-08-07 22:30:43 +0100

    tidy up logs

--- run-k8.sh
+++ run-k8.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Google stackdriver already gives us time and hostname
+export COLOREDLOGS_LOG_FORMAT='%(message)s'
+
 pipenv run python -m experiment.k8 "$@" \
 	--gcs-dir k8/mac-graph \
 	--bucket octavian-training \
