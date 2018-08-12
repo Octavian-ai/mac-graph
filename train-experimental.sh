f0e683b5cf4c0aadad8e9783b8ea096281fc438a
David Mack <david@sketchdeck.com>
2018-08-12 15:59:23 +0100

    experiment with adding network

--- train-experimental.sh
+++ train-experimental.sh
@@ -3,8 +3,18 @@
 COMMIT=$(git --no-pager log --pretty=format:'%h' -n 1)
 
 python -m macgraph.train \
-	--input-dir input_data/processed/sa-sp-small-100k \
-	--model-dir output/model/sa_sp/$COMMIT \
+	--input-dir input_data/processed/sa_small_100k_balanced \
+	--model-dir output/model/sa/$COMMIT \
 	--log-level DEBUG \
-	$@
-	# --enable-tf-debug \
+	--max-decode-iterations 1 \
+	--input-layers 1 \
+	--read-activation abs \
+	--read-dropout 0.0 \
+	--output-layers 1 \
+	--answer-classes 8 \
+	--input-width 64 \
+	--output-activation mi \
+	--learning-rate 0.001 \
+	--max-gradient-norm 4.0 \
+	--read-layers 1 \
+	--read-indicator-rows 1 \
