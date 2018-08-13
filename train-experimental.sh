6cd3d8ec1123eb3a2cace0cee3f893cc5f2c2cb6
David Mack <david@sketchdeck.com>
2018-08-13 22:41:23 +0100

    exp

--- train-experimental.sh
+++ train-experimental.sh
@@ -7,16 +7,20 @@ python -m macgraph.train \
 	--model-dir output/model/sp/$COMMIT \
 	--input-dir input_data/processed/sp_small_100k \
 	--disable-kb-edge \
-	--input-layers 1 \
+	--input-layers 3 \
 	--answer-classes 512 \
 	--vocab-size 512 \
 	--memory-transform-layers 1 \
-	--max-decode-iterations 2 \
-	--output-activation mi \
+	--max-decode-iterations 8 \
+	--output-activation tanh \
 	--output-layers 1 \
-	--read-activation abs \
+	--read-activation tanh \
 	--read-layers 1 \
-	--memory-forget-activation mi \
+	--memory-forget-activation tanh \
 	--control-dropout 0.0 \
 	--read-dropout 0.0 \
+	--input-width 64 \
+	--learning-rate 0.001 \
+	--max-gradient-norm 4 \
+	--disable-dynamic-decode \
 	$@
