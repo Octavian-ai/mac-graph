6f177708e2e0bdcc26047fa6d12db72901225712
David Mack <david@sketchdeck.com>
2018-08-15 14:55:14 +0100

    use tanh read act

--- floyd-train.sh
+++ floyd-train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RUNTIME=$(expr 60 \* 60 \* 1)
+RUNTIME=$(expr 60 \* 60 \* 2)
 COMMIT=$(git --no-pager log --pretty=format:'%h' -n 1)
 
 
@@ -16,63 +16,64 @@ COMMIT=$(git --no-pager log --pretty=format:'%h' -n 1)
 # 		--model-dir /output/model \
 # 	"
 
-floyd run \
-	--message "$COMMIT station properties" \
-	--cpu \
-	--env tensorflow-1.8 \
-	--data davidmack/datasets/mac-graph-station-properties:/input \
-	--max-runtime $RUNTIME \
-	"python -m macgraph.train \
-		--input-dir /input \
-		--output-dir /output \
-		--model-dir /output/model \
-		--disable-summary \
-		--disable-kb-edge \
-		--input-layers 3 \
-		--answer-classes 512 \
-		--vocab-size 512 \
-		--memory-transform-layers 1 \
-		--max-decode-iterations 8 \
-		--output-activation tanh \
-		--output-layers 1 \
-		--read-activation tanh \
-		--read-layers 1 \
-		--memory-forget-activation tanh \
-		--control-dropout 0.0 \
-		--read-dropout 0.0 \
-		--input-width 64 \
-		--learning-rate 0.001 \
-		--max-gradient-norm 4 \
-		--disable-dynamic-decode \
-	"
-
 # floyd run \
-# 	--message "$COMMIT station adjacency - verify max accuracy" \
+# 	--message "$COMMIT station properties" \
 # 	--cpu \
 # 	--env tensorflow-1.8 \
-# 	--data davidmack/datasets/mac-graph-station-adjacent:/input \
+# 	--data davidmack/datasets/mac-graph-station-properties:/input \
 # 	--max-runtime $RUNTIME \
 # 	"python -m macgraph.train \
 # 		--input-dir /input \
 # 		--output-dir /output \
 # 		--model-dir /output/model \
 # 		--disable-summary \
-# 		--disable-kb-node \
-# 		--max-decode-iterations 1 \
-# 		--input-layers 1 \
-# 		--disable-memory-cell \
-# 		--disable-control-cell \
-# 		--disable-dynamic-decode \
-# 		--disable-question-state \
-# 		--read-activation abs \
-# 		--read-from-question \
-# 		--read-dropout 0.0 \
+# 		--disable-kb-edge \
+# 		--input-layers 3 \
+# 		--answer-classes 512 \
+# 		--vocab-size 512 \
+# 		--memory-transform-layers 1 \
+# 		--max-decode-iterations 8 \
+# 		--output-activation tanh \
 # 		--output-layers 1 \
-# 		--answer-classes 8 \
-# 		--input-width 64 \
-# 		--output-activation mi \
-# 		--learning-rate 0.001 \
-# 		--max-gradient-norm 4.0 \
+# 		--read-activation tanh \
 # 		--read-layers 1 \
-# 		--read-indicator-rows 1 \
+# 		--memory-forget-activation tanh \
+# 		--control-dropout 0.0 \
+# 		--read-dropout 0.0 \
+# 		--input-width 64 \
+# 		--learning-rate 0.001 \
+# 		--max-gradient-norm 4 \
+# 		--disable-dynamic-decode \
 # 	"
+
+floyd run \
+	--message "$COMMIT station adjacency - try to converge with best known station properties" \
+	--cpu \
+	--env tensorflow-1.8 \
+	--data davidmack/datasets/mac-graph-station-adjacent:/input \
+	--max-runtime $RUNTIME \
+	"python -m macgraph.train \
+		--input-dir /input \
+		--output-dir /output \
+		--model-dir /output/model \
+		--disable-summary \
+		--disable-control-cell \
+		--disable-dynamic-decode \
+		--disable-memory-cell \
+		--disable-question-state \
+		--enable-read-question-state \
+		--input-layers 3 \
+		--input-width 64 \
+		--learning-rate 0.001 \
+		--max-decode-iterations 1 \
+		--max-gradient-norm 4.0 \
+		--output-activation mi \
+		--output-classes 512 \
+		--output-layers 1 \
+		--read-activation abs \
+		--read-dropout 0.0 \
+		--read-from-question \
+		--read-indicator-rows 1 \
+		--read-layers 1 \
+		--vocab-size 512 \
+	"
