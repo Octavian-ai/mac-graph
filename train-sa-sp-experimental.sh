f9450cf5a75cac47fceaeec8301cde88c0e16cf2
David Mack <david@sketchdeck.com>
2018-08-30 20:52:49 +0200

    try crazy

--- train-sa-sp-experimental.sh
+++ train-sa-sp-experimental.sh
@@ -2,19 +2,18 @@
 
 COMMIT=$(git --no-pager log --pretty=format:'%h' -n 1)
 
-MAX_STEPS=21
-
 python -m macgraph.train \
 	--model-dir output/model/sa_sp/exp/$COMMIT \
 	--input-dir input_data/processed/sa_sp_small_100k \
 	--control-dropout 0 \
 	--disable-dynamic-decode \
+	--disable-memory-cell \
 	--disable-question-state \
 	--enable-read-question-state \
-	--input-layers 1 \
+	--input-layers 3 \
 	--input-width 128 \
 	--learning-rate 0.001 \
-	--max-decode-iterations 2 \
+	--max-decode-iterations 1 \
 	--max-gradient-norm 0.4 \
 	--output-activation mi \
 	--output-classes 110 \
@@ -24,80 +23,7 @@ python -m macgraph.train \
 	--read-heads 1 \
 	--read-from-question \
 	--read-indicator-rows 1 \
-	--read-layers 3 \
+	--read-layers 1 \
 	--vocab-size 110 \
-	--max-steps $MAX_STEPS \
-	$@ \
-
-python -m macgraph.train \
-	--model-dir output/model/sa_sp/exp/just_sp/$COMMIT \
-	--input-dir input_data/processed/sp_small_100k \
-	--control-dropout 0 \
-	--disable-dynamic-decode \
-	--disable-question-state \
-	--enable-read-question-state \
-	--input-layers 1 \
-	--input-width 128 \
-	--learning-rate 0.001 \
-	--max-decode-iterations 2 \
-	--max-gradient-norm 0.4 \
-	--output-activation mi \
-	--output-classes 110 \
-	--output-layers 1 \
-	--read-activation tanh_abs \
-	--read-dropout 0 \
-	--read-heads 1 \
-	--read-from-question \
-	--read-indicator-rows 1 \
-	--read-layers 3 \
-	--vocab-size 110 \
-	--max-steps $MAX_STEPS \
-	$@ \
-
-python -m macgraph.train \
-	--model-dir output/model/sa_sp/exp/just_sa/$COMMIT \
-	--input-dir input_data/processed/sa_small_100k \
-	--control-dropout 0 \
-	--disable-dynamic-decode \
-	--disable-question-state \
-	--enable-read-question-state \
-	--input-layers 1 \
-	--input-width 128 \
-	--learning-rate 0.001 \
-	--max-decode-iterations 2 \
-	--max-gradient-norm 0.4 \
-	--output-activation mi \
-	--output-classes 110 \
-	--output-layers 1 \
-	--read-activation tanh_abs \
-	--read-dropout 0 \
-	--read-heads 1 \
-	--read-from-question \
-	--read-indicator-rows 1 \
-	--read-layers 3 \
-	--vocab-size 110 \
-	--max-steps $MAX_STEPS \
-	$@ \
-
-
-
-# python -m macgraph.train \
-# 	--model-dir output/model/sa_sp/exp/$COMMIT \
-# 	--input-dir input_data/processed/sa_sp_small_100k \
-# 	--disable-dynamic-decode \
-# 	--disable-question-state \
-# 	--input-layers 1 \
-# 	--input-width 64 \
-# 	--learning-rate 0.001 \
-# 	--max-decode-iterations 4 \
-# 	--max-gradient-norm 0.4 \
-# 	--output-activation tanh \
-# 	--output-classes 110 \
-# 	--output-layers 1 \
-# 	--read-activation tanh_abs \
-# 	--read-dropout 0.2 \
-# 	--read-from-question \
-# 	--read-indicator-rows 1 \
-# 	--read-layers 1 \
-# 	--disable-read-extract \
-# 	--vocab-size 110 \
+	--batch-size 8 \
+	$@
