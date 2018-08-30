6d5eb357a395bc6bae18948a0627b2763019e007
David Mack <david@sketchdeck.com>
2018-08-30 13:41:39 +0200

    try to get combined script working on sa

--- train-sa-sp-experimental.sh
+++ train-sa-sp-experimental.sh
@@ -5,15 +5,15 @@ COMMIT=$(git --no-pager log --pretty=format:'%h' -n 1)
 
 
 python -m macgraph.train \
-	--model-dir output/model/sa_sp/exp/$COMMIT \
-	--input-dir input_data/processed/sa_sp_small_100k \
+	--model-dir output/model/sa_sp/exp_just_sa/$COMMIT \
+	--input-dir input_data/processed/sa_small_100k \
 	--control-dropout 0 \
 	--disable-dynamic-decode \
 	--disable-memory-cell \
 	--disable-question-state \
 	--disable-summary \
 	--enable-read-question-state \
-	--input-layers 3 \
+	--input-layers 1 \
 	--input-width 128 \
 	--learning-rate 0.001 \
 	--max-decode-iterations 1 \
@@ -21,7 +21,7 @@ python -m macgraph.train \
 	--output-activation mi \
 	--output-classes 110 \
 	--output-layers 1 \
-	--read-activation tanh \
+	--read-activation tanh_abs \
 	--read-dropout 0 \
 	--read-heads 1 \
 	--read-from-question \
