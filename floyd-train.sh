62ead1a381ddc062bde280068f9f132c22f5e9c4
David Mack <david@sketchdeck.com>
2018-09-05 10:12:50 +0200

    more tfr printing

--- floyd-train.sh
+++ floyd-train.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-RUNTIME=$(expr 60 \* 60 \* 2)
+RUNTIME=$(expr 60 \* 60 \* 1)
 COMMIT=$(git --no-pager log --pretty=format:'%h' -n 1)
 
 
 floyd run \
-	--message "$COMMIT station properties + station adjacency" \
+	--message "$COMMIT station adjacency" \
 	--cpu \
 	--env tensorflow-1.8 \
 	--data davidmack/datasets/mac-graph-sa-sp:/input \
@@ -18,82 +18,55 @@ floyd run \
 		--disable-dynamic-decode \
 		--disable-memory-cell \
 		--disable-question-state \
-		--disable-summary \
 		--enable-read-question-state \
-		--input-layers 2 \
+		--input-layers 3 \
 		--input-width 128 \
 		--learning-rate 0.001 \
 		--max-decode-iterations 1 \
-		--max-gradient-norm 4.0 \
+		--max-gradient-norm 0.4 \
 		--output-activation mi \
-		--output-classes 512 \
+		--output-classes 110 \
 		--output-layers 1 \
-		--read-activation tanh \
+		--read-activation tanh_abs \
 		--read-dropout 0 \
+		--read-heads 1 \
 		--read-from-question \
 		--read-indicator-rows 1 \
 		--read-layers 1 \
-		--vocab-size 512 \
+		--vocab-size 110 \
+		--type-string-prefix StationAdjacent \
 	"
 
-# floyd run \
-# 	--message "$COMMIT station properties" \
-# 	--cpu \
-# 	--env tensorflow-1.8 \
-# 	--data davidmack/datasets/mac-graph-station-properties:/input \
-# 	--max-runtime $RUNTIME \
-# 	"python -m macgraph.train \
-# 		--input-dir /input \
-# 		--output-dir /output \
-# 		--model-dir /output/model \
-# 		--disable-summary \
-# 		--disable-kb-edge \
-# 		--input-layers 3 \
-# 		--answer-classes 512 \
-# 		--vocab-size 512 \
-# 		--memory-transform-layers 1 \
-# 		--max-decode-iterations 8 \
-# 		--output-activation tanh \
-# 		--output-layers 1 \
-# 		--read-activation tanh \
-# 		--read-layers 1 \
-# 		--memory-forget-activation tanh \
-# 		--control-dropout 0.0 \
-# 		--read-dropout 0.0 \
-# 		--input-width 64 \
-# 		--learning-rate 0.001 \
-# 		--max-gradient-norm 4 \
-# 		--disable-dynamic-decode \
-# 	"
 
-# floyd run \
-# 	--message "$COMMIT station adjacency - converged config" \
-# 	--cpu \
-# 	--env tensorflow-1.8 \
-# 	--data davidmack/datasets/mac-graph-station-adjacent:/input \
-# 	--max-runtime $RUNTIME \
-# 	"python -m macgraph.train \
-# 		--input-dir /input \
-# 		--output-dir /output \
-# 		--model-dir /output/model \
-# 		--disable-summary \
-# 	--disable-dynamic-decode \
-# 	--disable-memory-cell \
-# 	--disable-question-state \
-# 	--enable-read-question-state \
-# 	--control-dropout 0.0 \
-# 	--input-layers 3 \
-# 	--input-width 64 \
-# 	--learning-rate 0.001 \
-# 	--max-decode-iterations 1 \
-# 	--max-gradient-norm 4.0 \
-# 	--output-activation mi \
-# 	--output-classes 512 \
-# 	--output-layers 1 \
-# 	--read-activation mi \
-# 	--read-dropout 0.0 \
-# 	--read-from-question \
-# 	--read-indicator-rows 1 \
-# 	--read-layers 1 \
-# 	--vocab-size 512 \
-# 	"
+floyd run \
+	--message "$COMMIT station properties" \
+	--cpu \
+	--env tensorflow-1.8 \
+	--data davidmack/datasets/mac-graph-sa-sp:/input \
+	--max-runtime $RUNTIME \
+	"python -m macgraph.train \
+		--input-dir /input \
+		--output-dir /output \
+		--model-dir /output/model \
+		--control-dropout 0 \
+		--disable-dynamic-decode \
+		--disable-memory-cell \
+		--disable-question-state \
+		--enable-read-question-state \
+		--input-layers 3 \
+		--input-width 128 \
+		--learning-rate 0.001 \
+		--max-decode-iterations 1 \
+		--max-gradient-norm 0.4 \
+		--output-activation mi \
+		--output-classes 110 \
+		--output-layers 1 \
+		--read-activation tanh_abs \
+		--read-dropout 0 \
+		--read-heads 1 \
+		--read-from-question \
+		--read-indicator-rows 1 \
+		--read-layers 1 \
+		--vocab-size 110 \
+		--type-string-prefix StationProperty \
+	"
--- macgraph/input/print_tfr.py
+++ macgraph/input/print_tfr.py
@@ -2,35 +2,63 @@
 import tensorflow as tf
 import numpy as np
 
+from collections import Counter
+from tqdm import tqdm
+
 from .args import *
 from .util import *
-from .text_util import Vocab
+from .text_util import *
 from .input import *
+
+def eager_to_str(v):
+	return bytes_to_string(np.array(v))
+
+def extend_args(parser):
+	parser.add_argument('--print-records', action='store_true')
 
 if __name__ == "__main__":
 
-	args = get_args()
+	args = get_args(extend_args)
 
 	vocab = Vocab.load(args)
 	count = 0
 	tf.enable_eager_execution()
 
-	# Info about the experiment, for the record
-	for i in tf.python_io.tf_record_iterator(args["train_input_path"]):
+	dist = Counter()
 
-		if args["limit"] is None or count < args["limit"]:
-			r = parse_single_example(i)
-			r,label = reshape_example(args, r)
+	for i in tqdm(tf.python_io.tf_record_iterator(args["train_input_path"]), total=args["limit"]):
 
-			r["src"] = vocab.ids_to_english(np.array(r["src"]))
-			r["label"] = vocab.inverse_lookup(int(r["label"]))
-			r["kb_nodes"] = [vocab.ids_to_english(np.array(i)) for i in r["kb_nodes"] if np.array(i).size > 0]
+		if args["limit"] is not None and count > args["limit"]:
+			break
 
+		# Parse
+		r = parse_single_example(i)
+		r,label = reshape_example(args, r)
+		r["type_string"] = eager_to_str(r["type_string"])
+		r["src"] = vocab.ids_to_english(np.array(r["src"]))
+		r["label"] = vocab.inverse_lookup(int(r["label"]))
+		r["kb_nodes"] = [vocab.ids_to_english(np.array(i)) for i in r["kb_nodes"] if np.array(i).size > 0]
+
+		count += 1
+
+		# Skip non matching prefixes
+		if args["type_string_prefix"] is not None:
+			if not r["type_string"].startswith(args["type_string_prefix"]):
+				continue
+
+		dist[r["label"] + "/" + r["type_string"]] += 1
+
+		
+		if args["print_records"]:
 			print(r["src"] + " = " + r["label"])
 			for j in r["kb_nodes"]:
 				print("NODE: " + j)
 			print()
+		
+		
 
-		count += 1
+	print("\nDistribution:")
+	for k, v in dist.most_common():
+		print(k + "\t" + str(v))
 
-	print(f"Total records: {count}")
+	print(f"\nTotal records processed: {count}")
