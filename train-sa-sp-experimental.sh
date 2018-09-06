ea5f0e709168b594f55eaa5e7a80f712fcaad506
David Mack <david@sketchdeck.com>
2018-09-06 15:04:37 +0200

    try try again

--- train-sa-sp-experimental.sh
+++ train-sa-sp-experimental.sh
@@ -3,11 +3,11 @@
 COMMIT=$(git --no-pager log --pretty=format:'%h' -n 1)
 
 python -m macgraph.train \
-	--model-dir output/model/sa_sp/only_sp/$COMMIT \
-	--input-dir input_data/processed/junk \
+	--model-dir output/model/sa_sp/exp_subset/$COMMIT \
+	--input-dir input_data/processed/sa_sp_small_100k \
 	--disable-dynamic-decode \
 	--disable-memory-cell \
 	--disable-question-state \
 	--read-from-question \
-	--type-string-prefix StationAdj
+	--type-string-prefix StationProp \
 	$@
