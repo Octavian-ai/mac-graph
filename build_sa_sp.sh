286a7bcb04a60fc2ffafe0ebefa624fa845a5c40
David Mack <david@octavian.ai>
2018-09-06 12:27:30 +0000

    added stuff

--- build_sa_sp.sh
+++ build_sa_sp.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-
-python -m macgraph.input.build --gqa-path input_data/gqa-sa-sp-small-100k.yaml --input-dir input_data/sa-sp-small-100k/ --skip-vocab
+python -m macgraph.input.build --input-dir input_data/processed/default --gqa-path input_data/raw/gqa-sa-sp-small-100k.yaml --limit 1000000 --skip-vocab
