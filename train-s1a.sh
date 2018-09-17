a49eaf3ed1c2e277baf447ab20237c4addcfac9d
David Mack <david@sketchdeck.com>
2018-09-17 18:32:02 +0200

    size queen

--- train-s1a.sh
+++ train-s1a.sh
@@ -2,9 +2,11 @@
 
 COMMIT=$(git --no-pager log --pretty=format:'%h' -n 1)
 
+SIZE=10k
+
 python -m macgraph.train \
-	--model-dir output/model/s1a/$COMMIT \
-	--input-dir input_data/processed/s1a_small_5k \
+	--model-dir output/model/s1a/$SIZE/$COMMIT \
+	--input-dir input_data/processed/s1a_small_$SIZE \
 	--max-decode-iterations 2 \
 	--disable-kb-node \
 	--control-heads 2 \
