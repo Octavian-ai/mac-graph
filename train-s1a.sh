faad9bf8b366dccffac83aee97f668bd05e52fa6
David Mack <david@sketchdeck.com>
2018-09-17 17:47:48 +0200

    try with 5k

--- train-s1a.sh
+++ train-s1a.sh
@@ -4,7 +4,7 @@ COMMIT=$(git --no-pager log --pretty=format:'%h' -n 1)
 
 python -m macgraph.train \
 	--model-dir output/model/s1a/$COMMIT \
-	--input-dir input_data/processed/s1a_small_10k \
+	--input-dir input_data/processed/s1a_small_5k \
 	--max-decode-iterations 2 \
 	--disable-kb-node \
 	--control-heads 2 \
