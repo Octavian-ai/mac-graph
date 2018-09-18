2e596b3f06afe053cd62184b42502d588b883162
David Mack <david@sketchdeck.com>
2018-09-18 08:31:59 +0200

    use read heads

--- train-s1a.sh
+++ train-s1a.sh
@@ -7,8 +7,7 @@ SIZE=100k
 python -m macgraph.train \
 	--model-dir output/model/s1a/$SIZE/$COMMIT \
 	--input-dir input_data/processed/s1a_small_$SIZE \
-	--max-decode-iterations 2 \
-	--disable-kb-node \
+	--max-decode-iterations 6 \
 	--control-heads 2 \
-	--read-heads 2 \
+	--read-heads 1 \
 	$@
