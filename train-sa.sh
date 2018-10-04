96a2e4a511200e9ec63fc80592a87bbed2a83aff
David Mack <david@sketchdeck.com>
2018-10-04 15:48:00 -0700

    copy sa script from gold

--- train-sa.sh
+++ train-sa.sh
@@ -3,13 +3,11 @@
 COMMIT=$(git --no-pager log --pretty=format:'%h' -n 1)
 
 python -m macgraph.train \
-	--input-dir input_data/processed/sa_small_10k\
 	--model-dir output/model/sa/$COMMIT \
-	--disable-dynamic-decode \
+	--input-dir input_data/processed/sa_small_10k \
 	--max-decode-iterations 1 \
-	--disable-kb-node \
-	--disable-memory-cell \
 	--control-heads 2 \
-	--disable-summary \
 	--disable-message-passing \
+	--disable-memory-cell \
+	--disable-kb-node \
 	$@
