468597bfa07d1be9c2a5ec3e3d9df115f7fa3482
David Mack <david@sketchdeck.com>
2018-10-05 17:59:41 -0700

    latest script

--- train-s1a.sh
+++ train-s1a.sh
@@ -2,13 +2,16 @@
 
 COMMIT=$(git --no-pager log --pretty=format:'%h' -n 1)
 
-SIZE=10k
+SIZE=100k
 
 python -m macgraph.train \
 	--model-dir output/model/s1a/$SIZE/$COMMIT \
 	--input-dir input_data/processed/s1a_small_$SIZE \
-	--max-decode-iterations 6 \
+	--max-decode-iterations 2 \
+	--disable-dynamic-decode \
 	--control-heads 2 \
-	--read-heads 1 \
+	--disable-memory-cell \
 	--disable-read-cell \
+	--disable-input-bilstm \
+	--input-width 64 \
 	$@
