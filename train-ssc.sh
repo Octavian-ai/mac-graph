ef759c826c8cf54622cdcec99b0904e2f8c9d436
David Mack <david@sketchdeck.com>
2018-10-09 13:47:37 -0700

    one it no mp

--- train-ssc.sh
+++ train-ssc.sh
@@ -5,7 +5,8 @@ COMMIT=$(git --no-pager log --pretty=format:'%h' -n 1)
 python -m macgraph.train \
 	--model-dir output/model/ssc/$COMMIT \
 	--input-dir input_data/processed/ssc_small_1m \
-	--max-decode-iterations 2 \
+	--max-decode-iterations 1 \
+	--disable-message-passing \
 	--control-heads 2 \
 	--disable-input-bilstm \
 	--input-width 64 \
