2bd6c4c1f9c8f1af593bbb8484e03f980825343b
David Mack <david@sketchdeck.com>
2018-10-18 11:53:53 -0700

    5a

--- train-ssc.sh
+++ train-ssc.sh
@@ -3,20 +3,21 @@
 COMMIT=$(git --no-pager log --pretty=format:'%h' -n 1)
 
 python -m macgraph.train \
-	--model-dir output/model/ssc/4a/$COMMIT \
+	--model-dir output/model/ssc/5a/$COMMIT \
 	--input-dir input_data/processed/ssc_small_1m \
 	--filter-output-class 0 \
 	--filter-output-class 1 \
 	--filter-output-class 2 \
 	--filter-output-class 3 \
 	--filter-output-class 4 \
+	--filter-output-class 5 \
 	--control-heads 2 \
 	--disable-read-cell \
 	--disable-input-bilstm \
 	--input-width 32 \
 	--embed-width 32 \
 	--mp-state-width 5 \
-	--max-decode-iterations 5 \
+	--max-decode-iterations 6 \
 	--output-layers 1 \
 	--output-activation selu \
 	--disable-message-passing-node-transform \
