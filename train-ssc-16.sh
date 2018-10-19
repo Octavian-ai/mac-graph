62d9b804b5c1f51ca3de5b262c6d12cc8ffb6aef
David Mack <david@sketchdeck.com>
2018-10-18 18:57:18 -0700

    try gru on all

--- train-ssc-16.sh
+++ train-ssc-16.sh
@@ -3,18 +3,8 @@
 COMMIT=$(git --no-pager log --pretty=format:'%h' -n 1)
 
 python -m macgraph.train \
-	--model-dir output/model/ssc/9a/$COMMIT \
+	--model-dir output/model/ssc/16a/$COMMIT \
 	--input-dir input_data/processed/ssc_small_1m \
-	--filter-output-class 0 \
-	--filter-output-class 1 \
-	--filter-output-class 2 \
-	--filter-output-class 3 \
-	--filter-output-class 4 \
-	--filter-output-class 5 \
-	--filter-output-class 6 \
-	--filter-output-class 7 \
-	--filter-output-class 8 \
-	--filter-output-class 9 \
 	--control-heads 2 \
 	--disable-read-cell \
 	--disable-input-bilstm \
