1c353f74a431916f1cf78afdabc0bd838a047430
David Mack <david@sketchdeck.com>
2018-10-23 16:06:31 -0700

    Try other encode

--- train-spa.sh
+++ train-spa.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python -m macgraph.train --name spa-10k --max-decode-iterations 2
+python -m macgraph.train --name spa-10k --max-decode-iterations 3 --disable-input-lstm --embed-width 32
