ecdeb985725c09403b1f69526734ffcd4d3a7065
David Mack <david@sketchdeck.com>
2018-10-28 10:34:08 -0700

    one iter

--- train-spa.sh
+++ train-spa.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 python -m macgraph.train --name spa-10k \
-	--max-decode-iterations 4 \
+	--max-decode-iterations 1 \
 	--disable-input-bilstm \
 	--embed-width 128 \
 	--disable-message-passing \
