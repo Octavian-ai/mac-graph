6410c256a22c1694be343fc5f6179d1d50c8469d
David Mack <david@sketchdeck.com>
2018-11-21 22:09:24 -0800

    add build

---
+++ build-multistep.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+python -m macgraph.input.build \
+	--just-build-vocab \
+	--gqa-paths input_data/raw/* \
+	--name multistep
+
+for j in StationShortestCount StationShortestAvoidingCount StationTwoHops NearestStationArchitecture DistinctRoutes CountCycles
+do
+	nohup python -m macgraph.input.build \
+		--skip-vocab \
+		--vocab-path input_data/processed/multistep/vocab.txt \
+		--gqa-paths input_data/raw/gqa-$j* \
+		--name $j &
+done
