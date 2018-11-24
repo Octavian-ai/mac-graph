82fea7d69551a7744bb1a3522a7268ffffa2be99
David Mack <david@sketchdeck.com>
2018-11-24 11:55:41 -0800

    decode

--- macgraph/args.py
+++ macgraph/args.py
@@ -138,7 +138,7 @@ def get_args(extend=lambda parser:None, argv=None):
 	# Decode
 	# --------------------------------------------------------------------------
 	
-	parser.add_argument('--max-decode-iterations', 		type=int, default=1)
+	parser.add_argument('--max-decode-iterations', 		type=int, default=16)
 	parser.add_argument('--finished-steps-loss-factor',	type=float, default= 0.001)
 	parser.add_argument('--enable-dynamic-decode', 		action='store_true', dest="use_dynamic_decode")
 	parser.add_argument('--enable-independent-iterations', action='store_true', dest="use_independent_iterations")
