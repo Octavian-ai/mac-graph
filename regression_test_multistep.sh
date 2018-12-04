2134d6f501d33d536387279dc0c8a114abdc4f0f
David Mack <david@sketchdeck.com>
2018-12-03 21:07:11 -0800

    more stuff to run

--- regression_test_multistep.sh
+++ regression_test_multistep.sh
@@ -20,13 +20,26 @@ for task in "${tasks[@]}"
 do
 	for iteration in "${iterations[@]}"
 	do
+		# nohup python -m macgraph.regression_test \
+		# 	--name $task \
+		# 	--model-dir output/model/$task/$tag/$iteration \
+		# 	--tag $tag \
+		# 	--train-max-steps 50 \
+		# 	--max-decode-iterations $iteration \
+		# 	--enable-curriculum \
+		# 	--enable-comet &> nohup-$task-$iteration.out&
+
 		nohup python -m macgraph.regression_test \
 			--name $task \
-			--model-dir output/model/$task/$tag/$iteration \
-			--tag $tag \
+			--model-dir output/model/$task/curriculum_baseline/$iteration \
+			--tag curriculum_baseline \
 			--train-max-steps 50 \
 			--max-decode-iterations $iteration \
-			--enable-curriculum \
+			--filter-output-class 0 \
+			--filter-output-class 1 \
+			--filter-output-class 2 \
+			--filter-output-class 3 \
+			--filter-output-class 4 \
 			--enable-comet &> nohup-$task-$iteration.out&
 	done
 done
