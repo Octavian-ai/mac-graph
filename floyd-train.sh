4737d44555e64be9c1bb09f597a379e1f4b2eb46
David Mack <david@sketchdeck.com>
2018-12-09 15:14:31 -0800

    latest

--- floyd-train.sh
+++ floyd-train.sh
@@ -1,29 +1,100 @@
 #!/bin/bash
 
-iterations=1
-task=StationShortestCount
-tag=only_control
 
 RUNTIME=$(expr 60 \* 60 \* 4)
 COMMIT=$(git --no-pager log --pretty=format:'%h' -n 1)
 
 floyd run \
-	--message "$COMMIT $task $tag" \
+	--message "$COMMIT ssc mp variations" \
 	--cpu \
 	--env tensorflow-1.10 \
 	--data davidmack/datasets/mac-graph-ssc:/input \
-	--max-runtime $RUNTIME \
-	"python -m macgraph.regression_test \
-		--name $task \
+	"python -m macgraph.train \
+		--dataset StationShortestCount \
 		--input-dir /input \
 		--model-dir /output \
-		--tag $tag \
-		--train-max-steps 50 \
-		--max-decode-iterations $iterations \
-		--disable-memory-cell \
+		--max-decode-iterations 1 \
+		--train-max-steps 1 \
+		--tag upto_2 \
+		--tag iter_1 \
+		--tag mp_nid \
+		--filter-output-class 1 \
+		--filter-output-class 2 \
 		--disable-read-cell \
-		--disable-message-passing \
+		--control-heads 2 \
+		--disable-memory \
+		--enable-mp-node-id \
 		--enable-comet \
-		--enable-floyd \
-	"
+		--enable-floyd"
 
+
+floyd run \
+	--message "$COMMIT ssc mp variations" \
+	--cpu \
+	--env tensorflow-1.10 \
+	--data davidmack/datasets/mac-graph-ssc:/input \
+	"python -m macgraph.train \
+		--dataset StationShortestCount \
+		--input-dir /input \
+		--model-dir /output \
+		--max-decode-iterations 1 \
+		--train-max-steps 1 \
+		--tag upto_2 \
+		--tag iter_1 \
+		--tag mp_rs \
+		--filter-output-class 1 \
+		--filter-output-class 2 \
+		--disable-read-cell \
+		--control-heads 2 \
+		--disable-memory \
+		--enable-mp-right-shift \
+		--enable-comet \
+		--enable-floyd"
+
+
+floyd run \
+	--message "$COMMIT ssc mp variations" \
+	--cpu \
+	--env tensorflow-1.10 \
+	--data davidmack/datasets/mac-graph-ssc:/input \
+	"python -m macgraph.train \
+		--dataset StationShortestCount \
+		--input-dir /input \
+		--model-dir /output \
+		--max-decode-iterations 1 \
+		--train-max-steps 1 \
+		--tag upto_2 \
+		--tag iter_1 \
+		--tag mp_ngru \
+		--filter-output-class 1 \
+		--filter-output-class 2 \
+		--disable-read-cell \
+		--control-heads 2 \
+		--disable-memory \
+		--disable-mp-gru \
+		--enable-comet \
+		--enable-floyd"
+
+
+floyd run \
+	--message "$COMMIT ssc mp variations" \
+	--cpu \
+	--env tensorflow-1.10 \
+	--data davidmack/datasets/mac-graph-ssc:/input \
+	"python -m macgraph.train \
+		--dataset StationShortestCount \
+		--input-dir /input \
+		--model-dir /output \
+		--max-decode-iterations 1 \
+		--train-max-steps 1 \
+		--tag upto_2 \
+		--tag iter_1 \
+		--tag mp_wide \
+		--filter-output-class 1 \
+		--filter-output-class 2 \
+		--disable-read-cell \
+		--control-heads 2 \
+		--disable-memory \
+		--mp-state-width 128 \
+		--enable-comet \
+		--enable-floyd"
--- train-ssc-limited.sh
+++ train-ssc-limited.sh
@@ -6,64 +6,58 @@ python -m macgraph.train \
 	--train-max-steps 1 \
 	--tag upto_2 \
 	--tag iter_1 \
-	--tag no_read+memory \
 	--tag mp_nid \
 	--filter-output-class 1 \
 	--filter-output-class 2 \
 	--disable-read-cell \
 	--control-heads 2 \
 	--disable-memory \
-	--mp-activation linear \
 	--enable-mp-node-id \
 
 
-# python -m macgraph.train \
-# 	--dataset StationShortestCount \
-# 	--max-decode-iterations 3 \
-# 	--train-max-steps 50 \
-# 	--tag no_read_node \
-# 	--tag no_mp \
-# 	--tag upto_3 \
-# 	--tag iter_3 \
-# 	--filter-output-class 0 \
-# 	--filter-output-class 1 \
-# 	--filter-output-class 2 \
-# 	--filter-output-class 3 \
-# 	--disable-kb-node \
-# 	--disable-message-passing \
-# 	--learning-rate 1.96e-3 \
+python -m macgraph.train \
+	--dataset StationShortestCount \
+	--max-decode-iterations 1 \
+	--train-max-steps 1 \
+	--tag upto_2 \
+	--tag iter_1 \
+	--tag mp_rs \
+	--filter-output-class 1 \
+	--filter-output-class 2 \
+	--disable-read-cell \
+	--control-heads 2 \
+	--disable-memory \
+	--enable-mp-right-shift \
 
 
-# python -m macgraph.train \
-# 	--dataset StationShortestCount \
-# 	--max-decode-iterations 3 \
-# 	--train-max-steps 50 \
-# 	--tag lr_finder \
-# 	--tag no_read_node \
-# 	--tag no_mp \
-# 	--tag upto_3 \
-# 	--tag iter_3 \
-# 	--filter-output-class 0 \
-# 	--filter-output-class 1 \
-# 	--filter-output-class 2 \
-# 	--filter-output-class 3 \
-# 	--disable-kb-node \
-# 	--disable-message-passing \
-# 	--enable-lr-finder 
+python -m macgraph.train \
+	--dataset StationShortestCount \
+	--max-decode-iterations 1 \
+	--train-max-steps 1 \
+	--tag upto_2 \
+	--tag iter_1 \
+	--tag mp_ngru \
+	--filter-output-class 1 \
+	--filter-output-class 2 \
+	--disable-read-cell \
+	--control-heads 2 \
+	--disable-memory \
+	--disable-mp-gru \
 
 
-# python -m macgraph.train \
-# 	--dataset StationShortestCount \
-# 	--max-decode-iterations 3 \
-# 	--train-max-steps 50 \
-# 	--tag no_read \
-# 	--tag no_mp \
-# 	--tag upto_3 \
-# 	--tag iter_3 \
-# 	--filter-output-class 0 \
-# 	--filter-output-class 1 \
-# 	--filter-output-class 2 \
-# 	--filter-output-class 3 \
-# 	--disable-read-cell \
-# 	--disable-message-passing \
-# 	
+python -m macgraph.train \
+	--dataset StationShortestCount \
+	--max-decode-iterations 1 \
+	--train-max-steps 1 \
+	--tag upto_2 \
+	--tag iter_1 \
+	--tag mp_wide \
+	--filter-output-class 1 \
+	--filter-output-class 2 \
+	--disable-read-cell \
+	--control-heads 2 \
+	--disable-memory \
+	--mp-state-width 128 \
+
+
+
