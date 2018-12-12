5d74feb884ace24ae2aa21f87157a5865b4638a0
David Mack <david@sketchdeck.com>
2018-12-12 09:28:53 -0800

    train script ftw

--- train-ssc-limited.sh
+++ train-ssc-limited.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-
 nohup python -m macgraph.train \
 	--dataset StationShortestCount \
 	--max-decode-iterations 1 \
 	--train-max-steps 17 \
-	--tag upto_2 \
+	--tag upto_1 \
 	--tag iter_1 \
 	--tag mp_linear \
 	--filter-output-class 1 \
-	--filter-output-class 2 \
+	--filter-output-class 0 \
 	--disable-read-cell \
 	--control-heads 2 \
-	--disable-memory \
+	--disable-memory-cell \
+	--disable-read-cell \
 	--mp-activation linear \
 	--enable-comet &
 
@@ -20,14 +20,15 @@ nohup python -m macgraph.train \
 	--dataset StationShortestCount \
 	--max-decode-iterations 1 \
 	--train-max-steps 17 \
-	--tag upto_2 \
+	--tag upto_1 \
 	--tag iter_1 \
 	--tag mp_nid \
 	--filter-output-class 1 \
-	--filter-output-class 2 \
+	--filter-output-class 0 \
 	--disable-read-cell \
 	--control-heads 2 \
-	--disable-memory \
+	--disable-memory-cell \
+	--disable-read-cell \
 	--enable-mp-node-id \
 	--enable-comet &
 
@@ -36,14 +37,15 @@ nohup python -m macgraph.train \
 	--dataset StationShortestCount \
 	--max-decode-iterations 1 \
 	--train-max-steps 17 \
-	--tag upto_2 \
+	--tag upto_1 \
 	--tag iter_1 \
 	--tag mp_rs \
 	--filter-output-class 1 \
-	--filter-output-class 2 \
+	--filter-output-class 0 \
 	--disable-read-cell \
 	--control-heads 2 \
-	--disable-memory \
+	--disable-memory-cell \
+	--disable-read-cell \
 	--enable-mp-right-shift \
 	--enable-comet &
 
@@ -52,14 +54,15 @@ nohup python -m macgraph.train \
 	--dataset StationShortestCount \
 	--max-decode-iterations 1 \
 	--train-max-steps 17 \
-	--tag upto_2 \
+	--tag upto_1 \
 	--tag iter_1 \
 	--tag mp_ngru \
 	--filter-output-class 1 \
-	--filter-output-class 2 \
+	--filter-output-class 0 \
 	--disable-read-cell \
 	--control-heads 2 \
-	--disable-memory \
+	--disable-memory-cell \
+	--disable-read-cell \
 	--disable-mp-gru \
 	--enable-comet &
 
@@ -68,14 +71,15 @@ nohup python -m macgraph.train \
 	--dataset StationShortestCount \
 	--max-decode-iterations 1 \
 	--train-max-steps 17 \
-	--tag upto_2 \
+	--tag upto_1 \
 	--tag iter_1 \
 	--tag mp_wide \
 	--filter-output-class 1 \
-	--filter-output-class 2 \
+	--filter-output-class 0 \
 	--disable-read-cell \
 	--control-heads 2 \
-	--disable-memory \
+	--disable-memory-cell \
+	--disable-read-cell \
 	--mp-state-width 128 \
 	--enable-comet &
 
