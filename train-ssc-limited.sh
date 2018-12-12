5d62a2a1d57c555f5136050a4d4e3508262ae394
David Mack <david@sketchdeck.com>
2018-12-11 19:35:16 -0800

    fix script

--- train-ssc-limited.sh
+++ train-ssc-limited.sh
@@ -1,96 +1,83 @@
 #!/bin/bash
 
 
-python -m macgraph.train \
+nohup python -m macgraph.train \
 	--dataset StationShortestCount \
 	--max-decode-iterations 1 \
-	--train-max-steps 1 \
-	--tag check_if_code_works \
+	--train-max-steps 17 \
 	--tag upto_2 \
 	--tag iter_1 \
-	--tag mp_vanilla \
+	--tag mp_linear \
 	--filter-output-class 1 \
 	--filter-output-class 2 \
+	--disable-read-cell \
 	--control-heads 2 \
-	--disable-memory
+	--disable-memory \
+	--mp-activation linear \
+	--enable-comet &
 
-# nohup python -m macgraph.train \
-# 	--dataset StationShortestCount \
-# 	--max-decode-iterations 1 \
-# 	--train-max-steps 1 \
-# 	--tag upto_2 \
-# 	--tag iter_1 \
-# 	--tag mp_linear \
-# 	--filter-output-class 1 \
-# 	--filter-output-class 2 \
-# 	--disable-read-cell \
-# 	--control-heads 2 \
-# 	--disable-memory \
-# 	--mp-activation linear \
-# 	--enable-comet &
-
-# nohup python -m macgraph.train \
-# 	--dataset StationShortestCount \
-# 	--max-decode-iterations 1 \
-# 	--train-max-steps 1 \
-# 	--tag upto_2 \
-# 	--tag iter_1 \
-# 	--tag mp_nid \
-# 	--filter-output-class 1 \
-# 	--filter-output-class 2 \
-# 	--disable-read-cell \
-# 	--control-heads 2 \
-# 	--disable-memory \
-# 	--enable-mp-node-id \
-# 	--enable-comet &
+nohup python -m macgraph.train \
+	--dataset StationShortestCount \
+	--max-decode-iterations 1 \
+	--train-max-steps 17 \
+	--tag upto_2 \
+	--tag iter_1 \
+	--tag mp_nid \
+	--filter-output-class 1 \
+	--filter-output-class 2 \
+	--disable-read-cell \
+	--control-heads 2 \
+	--disable-memory \
+	--enable-mp-node-id \
+	--enable-comet &
 
 
-# nohup python -m macgraph.train \
-# 	--dataset StationShortestCount \
-# 	--max-decode-iterations 1 \
-# 	--train-max-steps 1 \
-# 	--tag upto_2 \
-# 	--tag iter_1 \
-# 	--tag mp_rs \
-# 	--filter-output-class 1 \
-# 	--filter-output-class 2 \
-# 	--disable-read-cell \
-# 	--control-heads 2 \
-# 	--disable-memory \
-# 	--enable-mp-right-shift \
-# 	--enable-comet &
+nohup python -m macgraph.train \
+	--dataset StationShortestCount \
+	--max-decode-iterations 1 \
+	--train-max-steps 17 \
+	--tag upto_2 \
+	--tag iter_1 \
+	--tag mp_rs \
+	--filter-output-class 1 \
+	--filter-output-class 2 \
+	--disable-read-cell \
+	--control-heads 2 \
+	--disable-memory \
+	--enable-mp-right-shift \
+	--enable-comet &
 
 
-# nohup python -m macgraph.train \
-# 	--dataset StationShortestCount \
-# 	--max-decode-iterations 1 \
-# 	--train-max-steps 1 \
-# 	--tag upto_2 \
-# 	--tag iter_1 \
-# 	--tag mp_ngru \
-# 	--filter-output-class 1 \
-# 	--filter-output-class 2 \
-# 	--disable-read-cell \
-# 	--control-heads 2 \
-# 	--disable-memory \
-# 	--disable-mp-gru \
-# 	--enable-comet &
+nohup python -m macgraph.train \
+	--dataset StationShortestCount \
+	--max-decode-iterations 1 \
+	--train-max-steps 17 \
+	--tag upto_2 \
+	--tag iter_1 \
+	--tag mp_ngru \
+	--filter-output-class 1 \
+	--filter-output-class 2 \
+	--disable-read-cell \
+	--control-heads 2 \
+	--disable-memory \
+	--disable-mp-gru \
+	--enable-comet &
 
 
-# nohup python -m macgraph.train \
-# 	--dataset StationShortestCount \
-# 	--max-decode-iterations 1 \
-# 	--train-max-steps 1 \
-# 	--tag upto_2 \
-# 	--tag iter_1 \
-# 	--tag mp_wide \
-# 	--filter-output-class 1 \
-# 	--filter-output-class 2 \
-# 	--disable-read-cell \
-# 	--control-heads 2 \
-# 	--disable-memory \
-# 	--mp-state-width 128 \
-# 	--enable-comet &
+nohup python -m macgraph.train \
+	--dataset StationShortestCount \
+	--max-decode-iterations 1 \
+	--train-max-steps 17 \
+	--tag upto_2 \
+	--tag iter_1 \
+	--tag mp_wide \
+	--filter-output-class 1 \
+	--filter-output-class 2 \
+	--disable-read-cell \
+	--control-heads 2 \
+	--disable-memory \
+	--mp-state-width 128 \
+	--enable-comet &
 
 
 
