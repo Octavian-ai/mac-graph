ee22e455dc01b16b77850170332c20553d5d489f
David Mack <david@sketchdeck.com>
2018-12-26 11:19:23 +0000

    works up to 32

--- train-ssc-limited.sh
+++ train-ssc-limited.sh
@@ -6,27 +6,16 @@
 i=1
 python -m macgraph.train \
 	--dataset StationShortestCount \
-	--max-decode-iterations 8 \
-	--train-max-steps 20 \
-	--tag upto_8 \
-	--tag iter_8 \
-	--tag curriculum \
-	--enable-curriculum \
-	--filter-output-class 8 \
-	--filter-output-class 7 \
-	--filter-output-class 6 \
-	--filter-output-class 5 \
-	--filter-output-class 4 \
-	--filter-output-class 3 \
-	--filter-output-class 2 \
-	--filter-output-class 1 \
-	--filter-output-class 0 \
+	--max-decode-iterations 2 \
+	--train-max-steps 2 \
+	--tag upto_2 \
+	--tag iter_2 \
 	--disable-control-cell \
 	--disable-read-cell \
 	--disable-memory-cell \
 	--disable-input-bilstm \
 	--enable-embed-const-eye \
-	--eval-every 90 \
+	--eval-every 500 \
 	--mp-state-width 1 \
 	--disable-mp-gru \
 	--input-width 128 \
